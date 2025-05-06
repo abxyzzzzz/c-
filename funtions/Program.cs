a77b63bd00c62a1a923f8e29f5939bb8cf5c3261
dhruv <superherosuperman1970@gmail.com>
2025-05-06 14:15:16 +0530

    odd and even

--- funtions/Program.cs
+++ funtions/Program.cs
@@ -6,7 +6,23 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Welcome to odd and even genrator ");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("please enter you nummber");
+            string number = Console.ReadLine();
+            int no = Convert.ToInt32(number);
+
+            if (no/2==0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+                {
+                    Console.WriteLine("odd");
+                }
+            }
         }
     }
-}
+
+
