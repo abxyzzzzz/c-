443754e4a3f52287ca1915237e0ea2d2f33182f0
dhruv <superherosuperman1970@gmail.com>
2025-05-06 13:53:37 +0530

    new folder funtions

---
+++ funtions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace funtions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
