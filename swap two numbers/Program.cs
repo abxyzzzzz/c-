b515284241c441134072a40f2ecc22d50c595a06
dhruv <superherosuperman1970@gmail.com>
2025-05-07 14:18:45 +0530

    swap two numbers

---
+++ swap two numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace swap_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello im number swapper ");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("press any enter/anykey to continue");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            char keyChar = keyInfo.KeyChar;
+            Console.WriteLine("");
+            Console.WriteLine("great lets continue");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("please enter the value/number for A:");
+            string no = Console.ReadLine();
+            int a = Convert.ToInt32(no);
+
+
+            Console.WriteLine("please enter the value/number for B:");
+            string no2 = Console.ReadLine();
+            int b = Convert.ToInt32(no2);
+
+
+        }
+    }
+}
