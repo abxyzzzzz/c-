e935959cacf9fcdc307c48a6216fcfae97d503e6
dhruv <superherosuperman1970@gmail.com>
2025-05-19 15:05:18 +0530

    changes

--- Fahrenheit To Celsius/Program.cs
+++ Fahrenheit To Celsius/Program.cs
@@ -8,39 +8,47 @@ namespace Fahrenheit_To_Celsius
 
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Hello im  fareinheit to celcuis converter and vice versa!");
-            Console.WriteLine("press cf to convert celciuis to farnheit");
-            Console.WriteLine("press fc to convert farnheit to celciuis");
-            Console.WriteLine("---------------------------------------------");
-
-            string p = Console.ReadLine();
-
-            if (p == "cf")
+            while (true)
             {
-                Console.WriteLine("Please enter your celcuis to convert to farnhiht");
-                string read = Console.ReadLine();
-                double c = Convert.ToDouble(read);
-                double cf = (c * 9 / 5) + 32;
-                Console.WriteLine($"your OUTPUT : {cf}");
+                Console.Clear();
+                Console.WriteLine("Hello im  fareinheit to celcuis converter and vice versa!");
+                Console.WriteLine("press cf to convert celciuis to farnheit");
+                Console.WriteLine("press fc to convert farnheit to celciuis");
+                Console.WriteLine("---------------------------------------------");
+                Console.Write(":");
 
+                string p = Console.ReadLine();
+                
+
+                if (p == "cf")
+                {
+                    Console.WriteLine("Please enter your celcuis to convert to farnhiht");
+                    string read = Console.ReadLine();
+                    double c = Convert.ToDouble(read);
+                    double cf = (c * 9 / 5) + 32;
+                    Console.WriteLine($"your OUTPUT : {cf}");
+
+                }
+
+                else if (p == "fc")
+                {
+                    Console.WriteLine("Please enter your farnehit to convert to celcuis");
+                    string readt = Console.ReadLine();
+                    double f = Convert.ToDouble(readt);
+                    double fc = (f - 32) / (9 / 5);
+                    Console.WriteLine($"your OUTPUT : {fc}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Eror");
+                    continue;
+
+                }
+                break;
             }
 
-            else if (p == "fc")
-            {
-                Console.WriteLine("Please enter your farnehit to convert to celcuis");
-                string readt = Console.ReadLine();
-                double f = Convert.ToDouble(readt);
-                double fc = (f - 32) / (9 / 5);
-                Console.WriteLine($"your OUTPUT : {fc}");
-            }
 
-            else {
-                Console.WriteLine("Eror");
-    
-            }
-    
-            
         }
     }
 }
