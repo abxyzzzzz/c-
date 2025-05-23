369430ec31e5213499bb08226e9e190a545592f6
dhruv <superherosuperman1970@gmail.com>
2025-05-23 16:12:22 +0530

    INHERITANCE FUNTION

--- ASCII Code/Program.cs
+++ ASCII Code/Program.cs
@@ -6,42 +6,7 @@ namespace ASCII_Code
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("Hello i am ASCAII code to DEC and vice versa");
-                Console.WriteLine("press (sd) for  SYMBOL(ASCAII) to DEC");
-                Console.WriteLine("press (ds) for  DEC to SYMBOL(ASCAII)");
-                string read = Console.ReadLine();
-
-                if (read == "sd" || read == "SD")
-                {
-                    Console.WriteLine("please enter the ASCAII code to convert into DEC");
-                    string r = Console.ReadLine();
-                    char character = r[0];
-                    int asc = Convert.ToInt32(character);
-                    Console.WriteLine($"OUPUT :{asc}");
-                    
-                }
-
-                else if (read == "ds" || read == "DS")
-                {
-                    Console.WriteLine("please enter the DEC code to convert into ASSCAII");
-                    string r = Console.ReadLine();
-                    int g = Convert.ToInt32(r);
-                    int f = g;
-                    char chrctr = (char)g;
-                    Console.WriteLine($"OUTPUT :{chrctr}");
-                    
-                }
-
-                else
-                {
-                    Console.WriteLine("invalid output please try again");
-                    continue;
-                }
-                break;
-            }
+            Console.WriteLine("");
 
         }
     }
---
+++ Inheritance funtion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Inheritance_funtion;
+
+namespace Inheritance_funtion
+{
+
+    public class vehicle
+    {
+        protected string name;
+        public vehicle(string name)
+        {
+            this.name = name;
+        }
+        public void AC()
+        {
+            Console.WriteLine($"{name} is turning on AC");
+        }
+        public void Brake()
+        {
+            Console.WriteLine($"{name} is pressing Brake");
+        }
+    }
+
+    public class audi : vehicle
+    {
+        public audi(string name) : base(name)
+        {
+            //constructur
+        }
+        public void Ai()
+        {
+            Console.WriteLine($"{name} ai has been activated");
+        }
+        public void SportsMode()
+        {
+            Console.WriteLine($"{name} SportsMode has been activated");
+        }
+    }
+    public class maruti : vehicle
+    {
+        public maruti(string name) : base(name)
+        {
+            //constructur
+        }
+        public void ManualTransmission()
+        {
+            Console.WriteLine($"{name} ManualTransmission is being used ");
+        }
+        public void Mileage()
+        {
+            Console.WriteLine($"{name} Mielage is good");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AUDI EXAMPLE");
+            audi newaudi = new audi("audi A4");
+            newaudi.Ai();
+            newaudi.AC();
+            newaudi.Brake();
+            newaudi.SportsMode();
+
+            Console.WriteLine();
+
+            Console.WriteLine("MARUTI EXAMPLE");
+            maruti newmaruti = new maruti("Maruti 800");
+            newmaruti.ManualTransmission();
+            newmaruti.Mileage();
+            newmaruti.Brake();
+            newmaruti.AC();
+
+
+        }
+    }
+}
