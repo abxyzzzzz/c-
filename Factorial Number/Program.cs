35699be4b1eeab033b23ffc8f5bb5c78c9763a20
dhruv <superherosuperman1970@gmail.com>
2025-05-29 16:05:13 +0530

    chnages

---
+++ Factorial Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Factorial_Number
+{
+    class Program
+    {
+
+          static long Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Factorial Number is not correct");
+            }
+
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial += i;
+            }
+
+            return factorial;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(4));
+        }
+    }
+}
