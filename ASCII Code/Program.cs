81cb082441a4bec50608fa7a77b5fcb5d89fcbc0
dhruv <superherosuperman1970@gmail.com>
2025-06-06 14:29:49 +0530

    ASCII code to DEC vice versa

--- ASCII Code/Program.cs
+++ ASCII Code/Program.cs
@@ -6,7 +6,33 @@ namespace ASCII_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Hello Im ASCII code to DEC and DEC to ASCII");
+            Console.WriteLine("Press 2 ASCII to DEC");
+            Console.WriteLine("Press 1 DEC to ASCII");
+            string read = Console.ReadLine();
+            int red = Convert.ToInt32(read);
+
+            if (red == 1)
+            {
+                Console.WriteLine("Enter your DEC number");
+                string st = Console.ReadLine();
+                int ints = Convert.ToInt32(st);
+                char character = (char)ints;
+                Console.WriteLine(character);
+            }
+            else if (red == 2)
+            {
+                string sgh = Console.ReadLine();
+                int as2=Convert.ToInt32(sgh);
+                int numbs = (int)as2;
+                Console.WriteLine(numbs);
+
+            }
+            else
+            {
+                Console.WriteLine("EORRRRR");
+            }
+
 
         }
     }
