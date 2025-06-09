d02cc4df801158ed9ff6e28280feff590a95b2bc
dhruv <superherosuperman1970@gmail.com>
2025-06-09 22:44:07 +0530

    Reversed String

---
+++ Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Plase enter your string and i will reverse it");
+            string read = Console.ReadLine();
+
+            Console.WriteLine(read.Reverse());
+        }
+    }
+}
