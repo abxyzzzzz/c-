5b1a26a033aeb424ef6696ae24271abc7417ddbe
dhruv <superherosuperman1970@gmail.com>
2025-06-10 21:56:21 +0530

    Reverse String

--- Reverse String/Program.cs
+++ Reverse String/Program.cs
@@ -9,8 +9,14 @@ namespace Reverse_String
         {
             Console.WriteLine("Hello Plase enter your string and i will reverse it");
             string read = Console.ReadLine();
+           
+            for (int i = read.Length - 1; i >= 0; i--)
+            {
+                Console.Write(read[i]);
+            }
+    
 
-            Console.WriteLine(read.Reverse());
+            //Console.WriteLine(read);
         }
     }
 }
