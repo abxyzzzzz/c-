e60374ca244fe57405a2ebaee548c2826e281ec4
dhruv <superherosuperman1970@gmail.com>
2025-06-11 14:44:05 +0530

    UPPER STRING TO LOWER ND VICE VERSA

---
+++ String to UPPER or LOWER/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace String_to_UPPER_or_LOWER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello Please enter \"U\" for UpperCase or \"L\" for LowerCase");
+            string read = Console.ReadLine();
+
+            if (read == "u" || read == "U")
+            {
+                Console.WriteLine("Please enter the text to convert to upper");
+                string u = Console.ReadLine();
+                Console.WriteLine(u.ToUpper());
+            }
+            else if (read == "l" || read == "L")
+            {
+                Console.WriteLine("Please enter the text to convert to lower");
+                string l = Console.ReadLine();
+                Console.WriteLine(l.ToLower());
+
+            }
+            else
+            {
+                Console.WriteLine("ERORRR");
+            }
+        }
+    }
+}
