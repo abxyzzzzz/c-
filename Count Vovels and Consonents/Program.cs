8c2bafe9471ee090a42f5b24f089ed25501b884f
dhruv <superherosuperman1970@gmail.com>
2025-06-11 14:32:23 +0530

    Vowels and Consonents

---
+++ Count Vovels and Consonents/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Count_Vovels_and_Consonents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Please Enter and i will count Vovels And Consonents ");
+            string vc = Console.ReadLine();
+            int len = vc.Length;
+            int Vovels = 0;
+            int Consonents = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (vc[i] == 'a' || vc[i] == 'e' || vc[i] == 'i' ||
+                    vc[i] == 'o' || vc[i] == 'u')
+                {
+                    Vovels++;
+                }
+                else
+                {
+                    Consonents++;
+                }
+            }
+            Console.WriteLine($"VOVELS ARE:{Vovels}");
+            Console.WriteLine($"CONSONENTS ARE:{ Consonents}");
+        }
+    }
+}
