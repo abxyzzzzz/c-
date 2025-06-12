e27ccae53ea4edde1ada25d17b0e0948fcbf28dd
dhruv <superherosuperman1970@gmail.com>
2025-06-12 20:59:06 +0530

    remove spaces from string

---
+++ Remove Spaces from String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Remove_Spaces_from_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello IM leading space remover i will remove your leading space");
+            Console.WriteLine("Please enter your text");
+
+            string text = Console.ReadLine();
+            Console.WriteLine($"OUTPUT: {text.Trim()}");
+        }
+    }
+}
