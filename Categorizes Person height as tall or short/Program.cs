1924991f866ba00ba7e9bfe0f60b390637b69e65
dhruv <superherosuperman1970@gmail.com>
2025-06-13 14:36:14 +0530

    Person height  or short

---
+++ Categorizes Person height as tall or short/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Categorizes_Person_height_as_tall_or_short
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello Im height teller wether your tall or short or average");
+            Console.WriteLine("Please tell your height in cm only");
+            string read = Console.ReadLine();
+            float value = Convert.ToSingle(read);
+
+            if (value <= 150)
+            {
+                Console.WriteLine("Your Short");
+
+            }
+            else if (value >= 151 || value < 165)
+            {
+                Console.WriteLine("Your Average");
+            }
+            else if (value >= 165 || value < 175)
+            {
+                Console.WriteLine("Your tall");
+            }
+            else if (value >= 175 || value < 210)
+            {
+                Console.WriteLine("YOU HAVE INSANE GENETICS!!!!!");
+            }
+
+        }
+    }
+}
